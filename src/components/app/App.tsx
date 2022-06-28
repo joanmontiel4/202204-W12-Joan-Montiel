b8ea329d7710d432e2a996930305754a9805720d
joanmontiel4 <joanmontiel@gmail.com>
2022-06-28 16:59:02 +0200

    Structuring components

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,19 @@
 import '../../css/styles.css';
+import { useContext } from 'react';
+import { CharacterContext } from '../../context/character.context';
+import { Card } from '../card/card';
 
 function App() {
-    return <></>;
+    const { characters } = useContext(CharacterContext);
+    return (
+        <ul className="characters-list row list-unstyled">
+            {characters.map((item) => {
+                <li key={item.name} className="character col">
+                    <Card></Card>
+                </li>;
+            })}
+        </ul>
+    );
 }
 
 export default App;
---
+++ src/components/card/card.tsx
@@ -0,0 +1,12 @@
+export function Card() {
+    return (
+        <div className="card character__card">
+            <img
+                src="img/no-one.jpg"
+                alt="Nombre y familia del personaje"
+                className="character__picture card-img-top"
+            />
+            <i className="emoji"></i>
+        </div>
+    );
+}
