d44f4087714a008062ea86fcc65545aa66dcf25d
joanmontiel4 <joanmontiel@gmail.com>
2022-06-29 13:59:11 +0200

    Add Card component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,17 +2,21 @@ import '../../css/styles.css';
 import { useContext } from 'react';
 import { CharacterContext } from '../../context/character.context';
 import { Card } from '../card/card';
+import { Comunications } from '../comunications/comunications';
 
 function App() {
     const { characters } = useContext(CharacterContext);
     return (
-        <ul className="characters-list row list-unstyled">
-            {characters.map((item) => {
-                <li key={item.name} className="character col">
-                    <Card></Card>
-                </li>;
-            })}
-        </ul>
+        <>
+            <ul className="characters-list row list-unstyled">
+                {characters.map((character) => (
+                    <li key={character.name} className="character col">
+                        <Card character={character}></Card>
+                    </li>
+                ))}
+            </ul>
+            <Comunications></Comunications>
+        </>
     );
 }
 
